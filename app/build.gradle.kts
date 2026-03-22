6631385ba439bea0a61199e0f46e1bd065dcc7c6
jhwon0812 <jhwon0812@naver.com>
2026-03-23 00:20:34 +0900

    1주차 미션파일

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
